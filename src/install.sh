a6b125bb9da52afb6e0206f9623a929820b838e4
eugene <eugenezador@gmail.com>
2024-01-26 14:37:46 +0300

    upd default perm for devises

--- src/install.sh
+++ src/install.sh
@@ -19,3 +19,17 @@ sudo apt update
 sudo apt install qtbase5-dev qt5-qmake
 # sudo apt install sofrware-properties-qt
 sudo pip3 install pyinstaller
+
+
+
+sudo adduser $USER dialout
+
+sudo touch /etc/udev/rules.d/50-myusb.rules
+
+# sudo sed "KERNEL==\"usbtmc[0-9]*\",MODE=\"0666\"" /etc/udev/rules.d/50-myusb.rules
+sudo chmod 777 /etc/udev/rules.d/50-myusb.rules
+sudo echo "KERNEL==\"usbtmc[0-9]*\",MODE=\"0666\"" >> /etc/udev/rules.d/50-myusb.rules
+sudo reboot
+
+
+
